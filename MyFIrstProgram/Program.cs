f7a9053fd7195ab63a4328dc7c8a3821d2394e04
Tyrese Samuel <s198046@students.aie.edu>
2019-08-19 15:01:05 -0500

    Add project files.

---
+++ MyFIrstProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFIrstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output text
+            
+            Console.WriteLine("What's ya name?\n");
+
+            int playerScore = 0;
+            string playerName = "Hit";
+
+            playerName = Console.ReadLine();
+
+            int add = 0;
+
+            while (playerScore < 10)
+            {
+                Console.Write("Increment score: \n");
+                add = Convert.ToInt32(Console.ReadLine());
+                playerScore = playerScore + add;
+                Console.Write("Current score: \n");
+                Console.WriteLine(playerScore);
+            }
+            
+            
+            Console.WriteLine("Score of " + playerName);
+            Console.WriteLine(playerScore);
+
+            Console.ReadLine();            
+        }
+    }
+}
